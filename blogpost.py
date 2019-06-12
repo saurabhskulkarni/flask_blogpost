5c5988b45f6aabe08e1db0b61d73bdd8ab36e3b2
Saurabh Kulkarni <saurabhkulkarni028@gmail.com>
2019-06-12 07:40:17 -0700

    First lesson updated

--- blogpost.py
+++ blogpost.py
@@ -1,1 +1,18 @@
-s
+#Importing flask
+from flask import Flask
+#Creating a variable app
+#Flask knows where do look for templates,etc
+app = Flask(__name__)
+
+
+#What we type into our browser is created using app decorators
+#Allows  us to write functions that will return text on our webpage
+@app.route("/")
+
+#Function to print something on our page
+def hello():
+    return "Hello World!"
+
+
+#NOTE
+#Before running this for the first time, set environment variable in terminal using "export FLASK_APP=flaskblog.py"
