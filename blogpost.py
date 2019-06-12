832d097932052b80ac6e4d91042ed00569ae7577
Saurabh Kulkarni <saurabhkulkarni028@gmail.com>
2019-06-12 08:08:24 -0700

    Completed Lesson 1

--- blogpost.py
+++ blogpost.py
@@ -7,12 +7,27 @@ app = Flask(__name__)
 
 #What we type into our browser is created using app decorators
 #Allows  us to write functions that will return text on our webpage
+#You can have two routes doing the same thing, leading to same page
 @app.route("/")
+@app.route("/home")
+#Function to print something on our page
+def home():
+    return "<h1>Hello Saurabh Kulkarni!</h1>"
+
+#Create an about route
+
+@app.route("/about")
 
 #Function to print something on our page
-def hello():
-    return "Hello World!"
+def about():
+    return "<h1>About Page</h1>"
 
 
 #NOTE
 #Before running this for the first time, set environment variable in terminal using "export FLASK_APP=flaskblog.py"
+#For your Ubuntu system, run conda activate to go into conda environment and then run flask run
+
+
+#To avoid setting app environment variables all the time when we want to run the program we do this
+if __name__=='__main__':
+    app.run(debug=True)
